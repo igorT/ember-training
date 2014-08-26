0e8ecdd674a6478ddcaf70a3aa8a8c1b90bf760f
IgorT <terzicigor@gmail.com>
2014-08-26 23:51:47 +0200

    before createRecord

--- app/controllers/cats.js
+++ app/controllers/cats.js
@@ -1,7 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
+  newName: '',
+  newImage: '',
   actions: {
+    createCat: function() {
+      var cat = this.store.createRecord('cat');
+      cat.set('name', this.get('newName'));
+      cat.set('image', this.get('newImage'));
+      this.set('newName', '');
+      this.set('newImage', '');
+      cat.save();
+    }
     /*
     delete: function(cat) {
       cat.deleteRecord();
