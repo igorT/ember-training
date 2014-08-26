e4103679205054f2d798d08d7791dbf46875196a
IgorT <terzicigor@gmail.com>
2014-08-26 22:46:13 +0200

    find implemented

--- app/adapters/cat.js
+++ app/adapters/cat.js
@@ -3,5 +3,8 @@ import DS from 'ember-data';
 export default DS.Adapter.extend({
   findAll: function(){
     return $.get('api/cat');
+  },
+  find: function(store, type, id, record){
+    return $.get('api/cat/' + id);
   }
 });
