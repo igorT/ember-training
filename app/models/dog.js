90cfed45d100df806de923c3c329d0e236ecae62
IgorT <terzicigor@gmail.com>
2014-08-27 06:01:30 +0200

    dog images

--- app/models/dog.js
+++ app/models/dog.js
@@ -2,6 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr(),
+  image: DS.attr(),
   playsFetch: DS.attr(),
   likesCats: DS.attr()
 });
--- server/mocks/dog.js
+++ server/mocks/dog.js
@@ -1,8 +1,9 @@
 module.exports = function(app) {
   var data = [
-      {id:"1",  name: 'dog1', image: "e7Fd7M1qejbiro1_1280.jpg", likes_cats: true, plays_fetch: false},
-      {id: "2", name: 'dog2', image: "http://24.media.tumblr.com/tumblr_lxrexcSVCh1qbd47zo1_1280.jpg", likes_cats: false, plays_fetch: true},
-      {id: "3", name: 'dog3', image: "http://24.media.tumblr.com/x3Rmp1Hjoou8ifx3UMrV8ILfo1_500.jpg", likes_cats: true, plays_fetch: true},
+      {id:"1",  name: 'dog1', image: "http://i3.kym-cdn.com/photos/images/newsfeed/000/217/040/48ACD.png", likes_cats: true, plays_fetch: false},
+      {id: "2", name: 'dog2', image: "http://www.ljplus.ru/img4/m/i/mister_trash/tumblr_lht7cvv39S1qamjl2o1_1280.jpg_AWSAccessKeyId_AKIAJ6IHWSU3BX3X7X3Q-Expires_1302934942-Signature_T1_2B9PPWHwEePu4hsLHTosJKnli4_3D", likes_cats: false, plays_fetch: true},
+      {id: "3", name: 'dog3', image: "http://i.imgur.com/sCcYF.jpg", likes_cats: true, plays_fetch: true},
+      {id: "4", name: 'dog4', image: "http://pawsdaily.com/wp-content/uploads/2013/12/today-has-been-ruff-dog-460x290.jpg", likes_cagts: true, plays_fetch: true}
     ];
   var express = require('express');
   var dogRouter = express.Router();
