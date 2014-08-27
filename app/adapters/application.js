e10ccd4a0db01c02e415a442078df36e89d69888
IgorT <terzicigor@gmail.com>
2014-08-27 05:27:49 +0200

    adapter ajax refactored, checkpoint

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,10 +1,13 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Adapter.extend({
   namespace: 'api',
+
   pathForType: function(type) {
     return type.typeKey;
   },
+
   buildUrl: function(type, id, record){
     if (!id) {
       return this.namespace + '/'+ this.pathForType(type);
@@ -14,37 +17,48 @@ export default DS.Adapter.extend({
 
   findAll: function(store, type){
     var url = this.buildUrl(type);
-    return $.get(url);
+    return this.ajax(url, 'get');
   },
 
   find: function(store, type, id, record){
     var url = this.buildUrl(type, id);
-    return $.get(url);
+    return this.ajax(url, 'get');
   },
 
   deleteRecord: function(store, type, record){
     var url = this.buildUrl(type, record.get('id'));
-    return $.ajax({
-      url: url,
-      type: 'delete'
-    });
+    return this.ajax(url, 'delete');
   },
 
   updateRecord: function(store, type, record){
     var url = this.buildUrl(type, record.get('id'));
-    return $.ajax({
-      url: url,
-      type: 'put',
-      data: record.serialize()
-    });
+    return this.ajax(url, 'put', record.serialize());
   },
 
   createRecord: function(store, type, record) {
     var url = this.buildUrl(type, record.get('id'));
-    return $.ajax({
-      url: url,
-      type: 'post',
-      data: record.serialize()
-    });
-  }
+    return this.ajax(url, 'post', record.serialize());
+  },
+
+  ajax: function(url, type, data) {
+
+    return new Ember.RSVP.Promise(function(resolve, reject) {
+      var options = { type: type, url: url};
+      if (data) {
+        options.data = data;
+      }
+
+      options.success = function(json) {
+        Ember.run(null, resolve, json);
+      };
+
+      options.error = function(jqXHR, textStatus, errorThrown) {
+        Ember.run(null, reject, jqXHR);
+      };
+
+      $.ajax(options);
+    }, "DS: RESTAdapter#ajax " + type + " to " + url);
+  },
+
+
 });
