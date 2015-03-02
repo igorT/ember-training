dc758f5fb27995a179a444d8843caa270f07398f
IgorT <terzicigor@gmail.com>
2015-03-02 15:36:50 -0800

    normalizing in the serializer

--- app/adapters/cat.js
+++ app/adapters/cat.js
@@ -3,19 +3,11 @@ import Ember from 'ember';
 
 export default DS.Adapter.extend({
   find: function(store, type, id) {
-    return Ember.$.get('/api/cats/' + id).then( (cat) => {
-      cat.ownerName = cat.owner_name;
-      return cat;
-   });
+    return Ember.$.get('/api/cats/' + id);
   },
 
   findAll: function() {
-    return Ember.$.get('/api/cats').then( (cats) => {
-       return cats.map( (cat) => {
-          cat.ownerName = cat.owner_name;
-          return cat;
-        });
-     });
+    return Ember.$.get('/api/cats');
   },
 
   updateRecord: function(store, type, record) {
