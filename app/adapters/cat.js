ba931900eb90c9c173ab991b9335cd3717ee42d8
IgorT <terzicigor@gmail.com>
2015-03-02 14:46:49 -0800

    find implemented

--- app/adapters/cat.js
+++ app/adapters/cat.js
@@ -2,6 +2,13 @@ import DS from 'ember-data';
 import Ember from 'ember';
 
 export default DS.Adapter.extend({
+  find: function(store, type, id) {
+    return Ember.$.get('/api/cats/' + id).then( (cat) => {
+      cat.ownerName = cat.owner_name;
+      return cat;
+   });
+  },
+
   findAll: function() {
     return Ember.$.get('/api/cats').then( (cats) => {
        return cats.map( (cat) => {
