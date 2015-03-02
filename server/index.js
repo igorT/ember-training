e687b9fe23385bee4887c492fdb3804a0ce94bd8
IgorT <terzicigor@gmail.com>
2015-03-02 07:30:55 -0800

    Add express body-parser back in

--- server/index.js
+++ server/index.js
@@ -14,7 +14,9 @@ module.exports = function(app) {
 
   // Log proxy requests
   var morgan  = require('morgan');
+  var bodyParser = require('body-parser');
   app.use(morgan('dev'));
+  app.use(bodyParser.json());
 
   mocks.forEach(function(route) { route(app); });
   proxies.forEach(function(route) { route(app); });
