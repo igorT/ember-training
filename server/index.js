3049263cc580fbb4710c387eea5a55456b0f5c36
IgorT <terzicigor@gmail.com>
2015-03-01 23:29:18 -0800

    Update to embercli 0.1.15

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
-// To use it create some files under `routes/`
-// e.g. `server/routes/ember-hamsters.js`
+// To use it create some files under `mocks/`
+// e.g. `server/mocks/ember-hamsters.js`
 //
 // module.exports = function(app) {
 //   app.get('/ember-hamsters', function(req, res) {
@@ -7,23 +7,16 @@
 //   });
 // };
 
-var bodyParser = require('body-parser');
-var globSync   = require('glob').sync;
-var mocks      = globSync('./mocks/**/*.js', { cwd: __dirname }).map(require);
-var proxies    = globSync('./proxies/**/*.js', { cwd: __dirname }).map(require);
-
 module.exports = function(app) {
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({
-    extended: true
-  }));
+  var globSync   = require('glob').sync;
+  var mocks      = globSync('./mocks/**/*.js', { cwd: __dirname }).map(require);
+  var proxies    = globSync('./proxies/**/*.js', { cwd: __dirname }).map(require);
 
-  mocks.forEach(function(route) { route(app)});
+  // Log proxy requests
+  var morgan  = require('morgan');
+  app.use(morgan('dev'));
 
-  // proxy expects a stream, but express will have turned
-  // the request stream into an object because bodyParser
-  // has run. We have to convert it back to stream:
-  // https://github.com/nodejitsu/node-http-proxy/issues/180
-  app.use(require('connect-restreamer')());
-  proxies.forEach(function(route) { route(app)});
+  mocks.forEach(function(route) { route(app); });
+  proxies.forEach(function(route) { route(app); });
+
 };
