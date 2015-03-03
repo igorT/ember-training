f6d5948f0672b1646d60dd0dbb38288b88522d06
IgorT <terzicigor@gmail.com>
2015-03-02 16:21:36 -0800

    basic serializer

--- app/serializers/cat.js
+++ app/serializers/cat.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Serializer.extend({
-  serialize: function(record) {
+  serialize: function(record, options) {
     return {
       id: record.get('id'),
       name: record.get('name'),
@@ -25,6 +25,10 @@ export default DS.Serializer.extend({
     if (requestType === 'findAll') {
       return this.extractFindAll(store,type, payload, id, requestType);
     } else if (requestType === 'find') {
+      return this.extractFindById(store,type, payload, id, requestType);
+    } else if (requestType === 'createRecord') {
+      return this.extractFindById(store,type, payload, id, requestType);
+    }  else if (requestType === 'updateRecord') {
       return this.extractFindById(store,type, payload, id, requestType);
     }
   }
