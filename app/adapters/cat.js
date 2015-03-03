df2cea94247306ed24a5b23d390d5630668ec501
IgorT <terzicigor@gmail.com>
2015-03-02 16:27:04 -0800

    buildURL

--- app/adapters/cat.js
+++ app/adapters/cat.js
@@ -2,27 +2,38 @@ import DS from 'ember-data';
 import Ember from 'ember';
 
 export default DS.Adapter.extend({
+  buildURL: function(type, id) {
+    if (id !== undefined && id !== null) {
+      return '/api/cats/' + id;
+    } else {
+      return '/api/cats/';
+    }
+  },
   find: function(store, type, id) {
-    return Ember.$.get('/api/cats/' + id);
+    var url = this.buildURL(type, id);
+    return Ember.$.get(url);
   },
 
   findAll: function() {
-    return Ember.$.get('/api/cats');
+    var url = this.buildURL(type, id);
+    return Ember.$.get(url);
   },
 
   updateRecord: function(store, type, record) {
+    var url = this.buildURL(type, record.get('id'));
     return Ember.$.ajax({
       type: 'put',
-      url: '/api/cats/' + record.get('id'),
+      url: url,
       data: JSON.stringify(record.serialize()),
       contentType: 'application/json',
       processData: false
    });
   },
   createRecord: function(store, type, record) {
+    var url = this.buildURL(type);
     return Ember.$.ajax({
       type: 'post',
-      url: '/api/cats/',
+      url: url,
       data: JSON.stringify(record.serialize()),
       contentType: 'application/json',
       processData: false
